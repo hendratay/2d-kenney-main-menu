28e5bacce440a1ca0c32244f1e5039fe8097fb79
hendratay <hendratay96@gmail.com>
2018-06-25 01:53:48 +0700

    setup sharedpreferences for music

--- app/src/main/java/com/example/hendratay/kenneymainmenu2d/MainActivity.kt
+++ app/src/main/java/com/example/hendratay/kenneymainmenu2d/MainActivity.kt
@@ -1,24 +1,19 @@
 package com.example.hendratay.kenneymainmenu2d
 
-import android.content.ComponentName
-import android.content.Context
-import android.content.Intent
-import android.content.ServiceConnection
+import android.content.*
 import android.os.Bundle
 import android.os.IBinder
-import android.support.v4.app.FragmentTransaction
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import com.firebase.ui.auth.AuthUI
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : BaseAcitivty() {
+class MainActivity : AppCompatActivity() {
 
     companion object {
-        val RC_SIGN_IN: Int = 123
+        const val RC_SIGN_IN: Int = 123
     }
 
-    val providers: List<AuthUI.IdpConfig> = arrayListOf(
+    private val providers: List<AuthUI.IdpConfig> = arrayListOf(
             AuthUI.IdpConfig.EmailBuilder().build(),
             AuthUI.IdpConfig.PhoneBuilder().build(),
             AuthUI.IdpConfig.GoogleBuilder().build(),
@@ -26,15 +21,44 @@ class MainActivity : BaseAcitivty() {
             AuthUI.IdpConfig.TwitterBuilder().build()
     )
 
+    var backgroundMusic: BackgroundMusic? = null
+    private val serviceConnection: ServiceConnection = object: ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            backgroundMusic = (service as BackgroundMusic.ServiceBinder).getService()
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            backgroundMusic = null
+        }
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bindService(Intent(this, BackgroundMusic::class.java), serviceConnection, Context.BIND_AUTO_CREATE)
 
-        setupMultiplayerButton()
+        setupMultiPlayerButton()
         setupOptionsButton()
     }
 
-    private fun setupMultiplayerButton() {
+    override fun onResume() {
+        super.onResume()
+        startService(Intent(this, BackgroundMusic::class.java))
+    }
+
+    override fun onPause() {
+        super.onPause()
+        backgroundMusic?.pauseMusic()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        stopService(Intent(this, BackgroundMusic::class.java))
+        unbindService(serviceConnection)
+    }
+
+    private fun setupMultiPlayerButton() {
         multiplayer.setOnClickListener {
             startActivityForResult(
                     AuthUI.getInstance()
--- app/src/main/java/com/example/hendratay/kenneymainmenu2d/OptionsDialog.kt
+++ app/src/main/java/com/example/hendratay/kenneymainmenu2d/OptionsDialog.kt
@@ -1,22 +1,40 @@
 package com.example.hendratay.kenneymainmenu2d
 
 import android.app.Dialog
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.DialogFragment
 import android.support.v7.app.AlertDialog
+import kotlinx.android.synthetic.main.fragment_options.view.*
 
 class OptionsDialog: DialogFragment() {
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val builder = AlertDialog.Builder(activity as MainActivity)
-        builder.setView((activity as MainActivity).layoutInflater.inflate(R.layout.fragment_options, null))
+        val view = (activity as MainActivity).layoutInflater.inflate(R.layout.fragment_options, null)
+        view.seekBar_music.progress = readSharedPreferences()
+        builder.setView(view)
                 .setPositiveButton("Save") { _, _ ->
-
+                    writeSharedPreferences(view.seekBar_music.progress)
                 }
                 .setNegativeButton("Cancel") { dialog, _ ->
                     dialog.dismiss()
                 }
         return builder.create()
+    }
+
+    private fun readSharedPreferences(): Int {
+        val sharedPref = activity?.getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE) ?: return 0
+        return sharedPref.getInt(getString(R.string.saved_music_volume), 50)
+    }
+
+    private fun writeSharedPreferences(progress: Int) {
+        val sharedPref = activity?.getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE) ?: return
+        with(sharedPref.edit()) {
+            putInt(getString(R.string.saved_music_volume), progress)
+            commit()
+        }
+        (activity as MainActivity).backgroundMusic?.setMusicVolume(readSharedPreferences() / 100f)
     }
 
 }
